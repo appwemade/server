3653ea8126c42f8ff3c3d57b9d798f16b9eee28b
appwemade <166166547+appwemade@users.noreply.github.com>
2025-05-31 10:27:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-/*
-*/
+
+// http://localhost:3000/fileWrite/123/
+// http://localhost:3000/fileRead/123/
+// http://localhost:3000/fileDelete/123/
 
 async function ᶠᶠappServer(){
     var ᵛᵒthrough = {};
@@ -56,8 +58,66 @@ async function ᶠᶠappServer__response(ᵛᵒrequest, ᵛᵒthrough){
         ᵛᵒrequest.ⁱᵒresponse.setHeader('Content-Type', 'application/json; charset=utf-8');
         ᵛᵒrequest.ⁱᵒresponse.setHeader('Access-Control-Allow-Credentials', 'true');
         ᵛᵒrequest.ⁱᵒresponse.setHeader('Access-Control-Allow-Origin', '*'); 
-    return ᵛᵒrequest.ⁱᵒresponse.end('{"status":'+Date.now() +'}');
+
+
+    //    var ᵛˢurl = ᵛᵒrequest.ⁱᵒrequest.url;
+    //    var ᵛᵃurl = ᵛˢurl.split('/');
+    //    console.log(ᵛᵃurl);
+
+
+    await ᶠᶠappServer__uri1(ᵛᵒrequest);
+    await ᶠᶠappServer__uri2(ᵛᵒrequest);
+    await ᶠᶠappServer__uri3(ᵛᵒrequest);
+
+
+    if(!ᵛᵒrequest?.ⁱᵒresponse?.finished ) return ᵛᵒrequest.ⁱᵒresponse.end('{"status":123}');
 };
+
+
+
+async function ᶠᶠappServer__uri1(ᵛᵒrequest){
+    if( ᵛᵒrequest?.ⁱᵒresponse?.finished ) return;
+    var ᵛˢurl = ᵛᵒrequest.ⁱᵒrequest.url;
+    var ᵛᵃurl = ᵛˢurl.split('/');
+    if(ᵛᵃurl?.[1] !== 'fileWrite' ) return;
+
+    var ᵛˢtext = 'hello ' + Date.now();
+    var ᵛᵛfs = require('fs');
+        ᵛᵛfs.writeFileSync('./test.txt', ᵛˢtext);
+
+    return ᵛᵒrequest.ⁱᵒresponse.end('{"uri":"fileWrite"}');
+};
+
+
+async function ᶠᶠappServer__uri2(ᵛᵒrequest){
+    if( ᵛᵒrequest?.ⁱᵒresponse?.finished ) return;
+    var ᵛˢurl = ᵛᵒrequest.ⁱᵒrequest.url;
+    var ᵛᵃurl = ᵛˢurl.split('/');
+    if(ᵛᵃurl?.[1] !== 'fileRead' ) return;
+
+    var ᵛˢtext = '';
+    var ᵛᵛfs = require('fs');
+    var ᵛˡfile = ᵛᵛfs.existsSync('./test.txt');
+    if( ᵛˡfile ) ᵛˢtext = ᵛᵛfs.readFileSync('./test.txt', 'utf8');
+
+    return ᵛᵒrequest.ⁱᵒresponse.end('{"text":"'+ᵛˢtext+'"}');
+};
+
+
+async function ᶠᶠappServer__uri3(ᵛᵒrequest){
+    if( ᵛᵒrequest?.ⁱᵒresponse?.finished ) return;
+    var ᵛˢurl = ᵛᵒrequest.ⁱᵒrequest.url;
+    var ᵛᵃurl = ᵛˢurl.split('/');
+    if(ᵛᵃurl?.[1] !== 'fileDelete' ) return;
+
+    var ᵛᵛfs = require('fs');
+    var ᵛˡfile = ᵛᵛfs.existsSync('./test.txt');
+    if( ᵛˡfile ) ᵛᵛfs.unlinkSync('./test.txt');
+
+    return ᵛᵒrequest.ⁱᵒresponse.end('{"uri":"fileDelete"}');
+};
+
+
 
 
 ᶠᶠappServer();
